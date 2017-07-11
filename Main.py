85c2cdd8335d86703858069fd8ec7bbdfa21f181
Jake Cheek <cheek.jake@gmail.com>
2017-07-11 16:08:27 -0400

    Notify user if duplicated jobs are added

--- Main.py
+++ Main.py
@@ -12,12 +12,15 @@ icon_path = os.path.join(resource_path, "icons")
 class MainUI(QWidget):
     def __init__(self):
         super(MainUI, self).__init__()
-
+        self.jobs_dict = dict()
         self.init_ui()
 
     def init_ui(self):
-        self.tray_icon = QSystemTrayIcon(QIcon(os.path.join(icon_path, "main.png")))
+        main_icon = QIcon(os.path.join(icon_path, "main.png"))
+        self.tray_icon = QSystemTrayIcon(main_icon)
         self.setWindowTitle("AutoDM")
+        self.setWindowIcon(main_icon)
+        self.resize(644, 405)
 
         self.create_controls()
         self.create_layout()
@@ -27,22 +30,18 @@ class MainUI(QWidget):
 
     def create_controls(self):
         self.job_num_edit = QLineEdit()
-
+        self.job_num_edit.setValidator(QIntValidator())
         self.job_num_add_button = QPushButton("Add")
-
         self.jobs_tab_widget = QTabWidget()
         self.jobs_tab_widget.setTabsClosable(True)
 
     def create_layout(self):
         main_layout = QVBoxLayout()
-
         job_num_layout = QHBoxLayout()
         job_num_layout.addWidget(self.job_num_edit)
         job_num_layout.addWidget(self.job_num_add_button)
         main_layout.addLayout(job_num_layout)
-
         main_layout.addWidget(self.jobs_tab_widget)
-
         self.setLayout(main_layout)
 
     def make_connections(self):
@@ -52,7 +51,20 @@ class MainUI(QWidget):
         self.tray_icon.activated.connect(self.restore_window)
 
     def find_job(self):
-        job_num = int(self.job_num_edit.text())
+        job_num = self.job_num_edit.text()
+        if job_num == "":
+            return
+        else:
+            job_num = int(job_num)
+
+        if self.jobs_dict.get(job_num, 0) > 0:
+            flags = QMessageBox.StandardButton.Yes
+            flags |= QMessageBox.StandardButton.No
+            response = QMessageBox.warning(self, "J{:d} Already Open".format(job_num),
+                                           "J{:d} is already open. Open a new instance anyway?".format(job_num), flags)
+            if response == QMessageBox.StandardButton.No:
+                return
+
         job_dir_finder = FindJobDir(job_num)
         job_dir_finder.finished.connect(self.add_job)
         try:
@@ -66,10 +78,14 @@ class MainUI(QWidget):
             pass
             # TODO: show error message
         else:
+            self.jobs_dict[num] = self.jobs_dict.get(num, 0) + 1
             tab_page = TabPage(job_dir)
             self.jobs_tab_widget.addTab(tab_page, "J" + str(num))
 
     def remove_job(self, index):
+        job_num = int(self.jobs_tab_widget.tabText(index)[1:])
+        self.jobs_dict[job_num] = self.jobs_dict.get(job_num) - 1
+
         widget = self.jobs_tab_widget.widget(index)
         if widget is not None:
             widget.deleteLater()
@@ -115,8 +131,8 @@ class TabPage(QWidget):
         # region Job Folders
         open_icon = QIcon(os.path.join(icon_path, "open.png"))
 
-        self.base_job_folder_edit = QLineEdit(self.job_dir)
-        self.base_job_folder_edit.setPlaceholderText("Base Job Folder")
+        self.base_job_folder_label = QLabel(self.job_dir)
+        # self.base_job_folder_label.setPlaceholderText("Base Job Folder")
 
         self.base_job_folder_button = QPushButton(open_icon, "")
 
@@ -173,7 +189,7 @@ class TabPage(QWidget):
         # region Job Folders
         job_folders_group = QGroupBox("Job Folders")
         job_folders_layout = QGridLayout()
-        job_folders_layout.addWidget(self.base_job_folder_edit, 0, 0)
+        job_folders_layout.addWidget(self.base_job_folder_label, 0, 0)
         job_folders_layout.addWidget(self.base_job_folder_button, 0, 1)
         job_folders_layout.addWidget(self.drone_folder_edit, 1, 0)
         job_folders_layout.addWidget(self.drone_folder_button, 1, 1)
