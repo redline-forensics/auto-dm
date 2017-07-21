510721d2f38375774b2584fba1af25bdbb8edaa1
Jake Cheek <cheek.jake@gmail.com>
2017-07-21 11:42:12 -0400

    Add open and close hotkeys and improve job folder controls

--- Main.py
+++ Main.py
@@ -1,15 +1,16 @@
 import os.path
+import os.path
 import sys
 
 from PySide.QtGui import QWidget, QSystemTrayIcon, QIcon, QApplication, QMenu, QLineEdit, QIntValidator, QPushButton, \
     QTabWidget, QHBoxLayout, QVBoxLayout, QMessageBox, QLabel, QFileDialog, QGroupBox, QGridLayout, QDesktopServices
-from JobType import JobType
 
 import Basecamp
 import Hotkeys
 import Pix4DBot
 from CustomWidgets import GoogleMapsStitcherDialog
 from JobDirFinder import *
+from JobType import JobType
 
 resource_path = os.path.join(os.path.split(__file__)[0], "resources")
 icon_path = os.path.join(resource_path, "icons")
@@ -53,8 +54,10 @@ class MainUI(QWidget):
 
     def init_hotkeys(self):
         # Format: Hotkeys.add_hotkey("J", ["Lcontrol", "Lwin"], self.woi)
-        Hotkeys.add_hotkey("J", ["Lcontrol", "Lwin"], self.add_job_hotkey)
-        Hotkeys.add_hotkey("T", ["Lcontrol", "Lwin"], self.test)
+        Hotkeys.add_hotkey("J", ["Lcontrol", "Lmenu"], self.add_job_hotkey)
+        Hotkeys.add_hotkey("O", ["Lcontrol", "Lmenu"], self.restore_window)
+        Hotkeys.add_hotkey("M", ["Lcontrol", "Lmenu"], self.showMinimized)
+        Hotkeys.add_hotkey("T", ["Lcontrol", "Lmenu"], self.test)
 
     def test(self):
         self.restore_window()
@@ -63,6 +66,7 @@ class MainUI(QWidget):
             test_tab.run_pix4d_bot_site()
 
     def add_job_hotkey(self):
+        self.showMinimized()
         self.restore_window()
         self.job_num_edit.setFocus()
         self.job_num_edit.selectAll()
@@ -114,7 +118,7 @@ class MainUI(QWidget):
                 return
 
         self.jobs_dict[num] = self.jobs_dict.get(num, 0) + 1
-        tab_page = TabPage(num, job_dir)
+        tab_page = TabPage(num, job_dir, self)
         self.jobs_tab_widget.insertTab(0, tab_page, "J" + str(num))
         self.jobs_tab_widget.setCurrentIndex(0)
         return tab_page
@@ -136,9 +140,6 @@ class MainUI(QWidget):
         else:
             return super(MainUI, self).event(event)
 
-    def minimize(self):
-        self.showMinimized()
-
     def closeEvent(self, event):
         reply = QMessageBox.question(self, "Message", "Are you sure you want to quit?",
                                      QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
@@ -157,16 +158,16 @@ class MainUI(QWidget):
             self.activateWindow()
 
 
-class TabPage(QWidget):
+class TabPage(QWidget, object):
     # region job_dir
     @property
     def job_dir(self):
         return self.__job_dir
 
     @job_dir.setter
-    def job_dir(self, job_dir):
-        self.__job_dir = job_dir
-        self.base_job_folder.setText(job_dir)
+    def job_dir(self, value):
+        self.__job_dir = value
+        self.base_job_folder.setText(value)
 
     # endregion
     # region drone_dir
@@ -175,9 +176,16 @@ class TabPage(QWidget):
         return self.__drone_dir
 
     @drone_dir.setter
-    def drone_dir(self, drone_dir):
-        self.__drone_dir = drone_dir
-        self.drone_folder_edit.setText(drone_dir)
+    def drone_dir(self, value):
+        self.__drone_dir = value
+
+        if self.drone_folder_edit.text() != value:
+            self.drone_folder_edit.setText(value)
+
+        if os.path.isdir(value):
+            self.drone_folder_open_button.setVisible(True)
+        else:
+            self.drone_folder_open_button.setVisible(False)
 
     # endregion
     # region scans_dir
@@ -186,9 +194,16 @@ class TabPage(QWidget):
         return self.__scans_dir
 
     @scans_dir.setter
-    def scans_dir(self, scans_dir):
-        self.__scans_dir = scans_dir
-        self.scans_folder_edit.setText(scans_dir)
+    def scans_dir(self, value):
+        self.__scans_dir = value
+
+        if self.scans_folder_edit.text() != value:
+            self.scans_folder_edit.setText(value)
+
+        if os.path.isdir(value):
+            self.scans_folder_open_button.setVisible(True)
+        else:
+            self.scans_folder_open_button.setVisible(False)
 
     # endregion
     # region assets_dir
@@ -197,19 +212,31 @@ class TabPage(QWidget):
         return self.__assets_dir
 
     @assets_dir.setter
-    def assets_dir(self, assets_dir):
-        self.__assets_dir = assets_dir
-        self.assets_folder_edit.setText(assets_dir)
+    def assets_dir(self, value):
+        self.__assets_dir = value
+
+        if self.assets_folder_edit.text() != value:
+            self.assets_folder_edit.setText(value)
+
+        if value == "":
+            self.assets_folder_add_button.setVisible(True)
+        else:
+            if os.path.isdir(value):
+                self.assets_folder_open_button.setVisible(True)
+            else:
+                self.assets_folder_open_button.setVisible(False)
+            self.assets_folder_add_button.setVisible(False)
 
     # endregion
 
-    def __init__(self, job_num, job_dir):
+    def __init__(self, job_num, job_dir, main_window):
         super(TabPage, self).__init__()
         self.basecamp_url = Basecamp.get_basecamp_page(job_num)
         self.init_bots()
         self.init_ui()
         self.find_dirs(job_dir)
         self.job_num = job_num
+        self.main_window = main_window
 
     def init_bots(self):
         self.pix4d_bot = None
@@ -221,6 +248,7 @@ class TabPage(QWidget):
 
     def create_controls(self):
         # region Job Folders
+        edit_icon = QIcon(os.path.join(icon_path, "edit.png"))
         open_icon = QIcon(os.path.join(icon_path, "open.png"))
 
         self.base_job_folder_label = QLabel("Base:")
@@ -233,19 +261,22 @@ class TabPage(QWidget):
         self.drone_folder_label = QLabel("{}:".format(drone_str))
         self.drone_folder_edit = QLineEdit()
         self.drone_folder_edit.setPlaceholderText("{} {}".format(drone_str, folder_str))
-        self.drone_folder_button = QPushButton(open_icon, "")
+        self.drone_folder_edit_button = QPushButton(edit_icon, "")
+        self.drone_folder_open_button = QPushButton(open_icon, "")
 
         scans_str = "Scans"
         self.scans_folder_label = QLabel("{}:".format(scans_str))
         self.scans_folder_edit = QLineEdit()
         self.scans_folder_edit.setPlaceholderText("{} {}".format(scans_str, folder_str))
-        self.scans_folder_button = QPushButton(open_icon, "")
+        self.scans_folder_edit_button = QPushButton(edit_icon, "")
+        self.scans_folder_open_button = QPushButton(open_icon, "")
 
         assets_str = "Assets"
         self.assets_folder_label = QLabel("{}:".format(assets_str))
         self.assets_folder_edit = QLineEdit()
         self.assets_folder_edit.setPlaceholderText("{} {}".format(assets_str, folder_str))
-        self.assets_folder_button = QPushButton(open_icon, "")
+        self.assets_folder_edit_button = QPushButton(edit_icon, "")
+        self.assets_folder_open_button = QPushButton(open_icon, "")
         self.assets_folder_add_button = QPushButton(QIcon(os.path.join(icon_path, "new_dir.png")), "")
         # endregion
         # region Job Controls
@@ -290,7 +321,8 @@ class TabPage(QWidget):
         job_folders_layout.addWidget(self.drone_folder_label, 1, 0)
         drone_folder_layout = QHBoxLayout()
         drone_folder_layout.addWidget(self.drone_folder_edit)
-        drone_folder_layout.addWidget(self.drone_folder_button)
+        drone_folder_layout.addWidget(self.drone_folder_edit_button)
+        drone_folder_layout.addWidget(self.drone_folder_open_button)
         drone_folder_layout.setStretch(0, 1)
         drone_folder_layout.setStretch(1, 0)
         job_folders_layout.addLayout(drone_folder_layout, 1, 1)
@@ -298,7 +330,8 @@ class TabPage(QWidget):
         job_folders_layout.addWidget(self.scans_folder_label, 2, 0)
         scans_folder_layout = QHBoxLayout()
         scans_folder_layout.addWidget(self.scans_folder_edit)
-        scans_folder_layout.addWidget(self.scans_folder_button)
+        scans_folder_layout.addWidget(self.scans_folder_edit_button)
+        scans_folder_layout.addWidget(self.scans_folder_open_button)
         scans_folder_layout.setStretch(0, 1)
         scans_folder_layout.setStretch(1, 0)
         job_folders_layout.addLayout(scans_folder_layout, 2, 1)
@@ -306,7 +339,8 @@ class TabPage(QWidget):
         job_folders_layout.addWidget(self.assets_folder_label, 3, 0)
         assets_folder_layout = QHBoxLayout()
         assets_folder_layout.addWidget(self.assets_folder_edit)
-        assets_folder_layout.addWidget(self.assets_folder_button)
+        assets_folder_layout.addWidget(self.assets_folder_edit_button)
+        assets_folder_layout.addWidget(self.assets_folder_open_button)
         assets_folder_layout.addWidget(self.assets_folder_add_button)
         assets_folder_layout.setStretch(0, 1)
         assets_folder_layout.setStretch(1, 0)
@@ -391,13 +425,25 @@ class TabPage(QWidget):
         self.setLayout(main_layout)
 
     def make_connections(self):
-        self.assets_folder_edit.textChanged.connect(lambda text: self.assets_folder_add_button.setVisible(text == ""))
-        self.drone_folder_button.clicked.connect(self.choose_drone_dir)
-        self.scans_folder_button.clicked.connect(self.choose_scans_dir)
-        self.assets_folder_button.clicked.connect(self.choose_assets_dir)
+        self.drone_folder_edit.returnPressed.connect(lambda: setattr(self, "drone_dir", self.drone_folder_edit.text()))
+        self.scans_folder_edit.returnPressed.connect(lambda: setattr(self, "scans_dir", self.scans_folder_edit.text()))
+        self.assets_folder_edit.returnPressed.connect(
+            lambda: setattr(self, "assets_dir", self.assets_folder_edit.text()))
+
+        self.drone_folder_edit_button.clicked.connect(
+            lambda: setattr(self, "drone_dir", self.choose_dir(self.drone_dir)))
+        self.scans_folder_edit_button.clicked.connect(
+            lambda: setattr(self, "scans_dir", self.choose_dir(self.scans_dir)))
+        self.assets_folder_edit_button.clicked.connect(
+            lambda: setattr(self, "assets_dir", self.choose_dir(self.assets_dir)))
+
         self.assets_folder_add_button.clicked.connect(self.create_assets_dir)
 
-        self.open_job_folder_button.clicked.connect(self.open_job_folder)
+        self.drone_folder_open_button.clicked.connect(lambda: self.open_folder(self.drone_dir))
+        self.scans_folder_open_button.clicked.connect(lambda: self.open_folder(self.scans_dir))
+        self.assets_folder_open_button.clicked.connect(lambda: self.open_folder(self.assets_dir))
+
+        self.open_job_folder_button.clicked.connect(lambda: self.open_folder(self.job_dir))
         self.open_basecamp_page_button.clicked.connect(self.open_basecamp_page)
 
         self.pix4d_site_button.clicked.connect(self.run_pix4d_bot_site)
@@ -406,62 +452,62 @@ class TabPage(QWidget):
         self.google_maps_stitcher_button.clicked.connect(self.show_google_maps_stitcher)
 
     def find_dirs(self, job_dir):
+        def find_drone_dir():
+            charlotte_drone_path = os.path.join(self.job_dir, "Drone")
+            if os.path.isdir(charlotte_drone_path):
+                self.drone_dir = charlotte_drone_path
+                return
+
+            nashville_drone_path = os.path.join(self.job_dir, *["Photographs", "Drone"])
+            if os.path.isdir(nashville_drone_path):
+                self.drone_dir = nashville_drone_path
+                return
+
+            self.drone_dir = ""
+
+        def find_scans_dir():
+            charlotte_scans_path = os.path.join(self.job_dir, "Scans")
+            if os.path.isdir(charlotte_scans_path):
+                self.scans_dir = charlotte_scans_path
+                return
+
+            nashville_scans_path_0 = os.path.join(self.job_dir, "Scan Data")
+            if os.path.isdir(nashville_scans_path_0):
+                self.scans_dir = nashville_scans_path_0
+                return
+
+            nashville_scans_path_1 = os.path.join(self.job_dir, "Scanned Data")
+            if os.path.isdir(nashville_scans_path_1):
+                self.scans_dir = nashville_scans_path_1
+                return
+
+            self.scans_dir = ""
+
+        def find_assets_dir():
+            charlotte_drawings_path = os.path.join(self.job_dir, "Drawings")
+            charlotte_drawing_path = os.path.join(self.job_dir, "Drawing")
+            if os.path.isdir(charlotte_drawings_path):
+                drawings_path = charlotte_drawings_path
+            elif os.path.isdir(charlotte_drawing_path):
+                drawings_path = charlotte_drawing_path
+            else:
+                self.assets_dir = ""
+                return
+
+            charlotte_assets_path = os.path.join(drawings_path, "Assets")
+            charlotte_asset_path = os.path.join(drawings_path, "Asset")
+            if os.path.isdir(charlotte_assets_path):
+                self.assets_dir = charlotte_assets_path
+            elif os.path.isdir(charlotte_asset_path):
+                self.assets_dir = charlotte_asset_path
+            else:
+                self.assets_dir = drawings_path
+                return
+
         self.job_dir = job_dir
-        self.find_drone_dir()
-        self.find_scans_dir()
-        self.find_assets_dir()
-
-    def find_drone_dir(self):
-        charlotte_drone_path = os.path.join(self.job_dir, "Drone")
-        if os.path.isdir(charlotte_drone_path):
-            self.drone_dir = charlotte_drone_path
-            return
-
-        nashville_drone_path = os.path.join(self.job_dir, *["Photographs", "Drone"])
-        if os.path.isdir(nashville_drone_path):
-            self.drone_dir = nashville_drone_path
-            return
-
-        self.drone_dir = ""
-
-    def find_scans_dir(self):
-        charlotte_scans_path = os.path.join(self.job_dir, "Scans")
-        if os.path.isdir(charlotte_scans_path):
-            self.scans_dir = charlotte_scans_path
-            return
-
-        nashville_scans_path_0 = os.path.join(self.job_dir, "Scan Data")
-        if os.path.isdir(nashville_scans_path_0):
-            self.scans_dir = nashville_scans_path_0
-            return
-
-        nashville_scans_path_1 = os.path.join(self.job_dir, "Scanned Data")
-        if os.path.isdir(nashville_scans_path_1):
-            self.scans_dir = nashville_scans_path_1
-            return
-
-        self.scans_dir = ""
-
-    def find_assets_dir(self):
-        charlotte_drawings_path = os.path.join(self.job_dir, "Drawings")
-        charlotte_drawing_path = os.path.join(self.job_dir, "Drawing")
-        if os.path.isdir(charlotte_drawings_path):
-            drawings_path = charlotte_drawings_path
-        elif os.path.isdir(charlotte_drawing_path):
-            drawings_path = charlotte_drawing_path
-        else:
-            self.assets_dir = ""
-            return
-
-        charlotte_assets_path = os.path.join(drawings_path, "Assets")
-        charlotte_asset_path = os.path.join(drawings_path, "Asset")
-        if os.path.isdir(charlotte_assets_path):
-            self.assets_dir = charlotte_assets_path
-        elif os.path.isdir(charlotte_asset_path):
-            self.assets_dir = charlotte_asset_path
-        else:
-            self.assets_dir = ""
-            return
+        find_drone_dir()
+        find_scans_dir()
+        find_assets_dir()
 
     def create_assets_dir(self):
         charlotte_drawings_path = os.path.join(self.job_dir, "Drawings")
@@ -484,23 +530,24 @@ class TabPage(QWidget):
             os.makedirs(charlotte_assets_path)
             self.assets_dir = charlotte_assets_path
 
-    def choose_drone_dir(self):
-        choose_dir = QFileDialog.getExistingDirectory(self, "Choose Drone Folder", self.job_dir)
-        if choose_dir != "":
-            self.drone_dir = choose_dir
+    def choose_dir(self, dir_var):
+        if dir_var == self.drone_dir:
+            dir_str = "Drone"
+        elif dir_var == self.scans_dir:
+            dir_str = "Scans"
+        elif dir_var == self.assets_dir:
+            dir_str = "Assets"
+        else:
+            dir_str = ""
 
-    def choose_scans_dir(self):
-        choose_dir = QFileDialog.getExistingDirectory(self, "Choose Scans Folder", self.job_dir)
-        if choose_dir != "":
-            self.scans_dir = choose_dir
+        choose_dir = QFileDialog.getExistingDirectory(self, "Choose {} Folder".format(dir_str), self.job_dir)
+        if choose_dir == "":
+            return dir_var
+        return choose_dir
 
-    def choose_assets_dir(self):
-        choose_dir = QFileDialog.getExistingDirectory(self, "Choose Assets Folder", self.job_dir)
-        if choose_dir != "":
-            self.assets_dir = choose_dir
-
-    def open_job_folder(self):
-        QDesktopServices.openUrl(QUrl("file:{}".format(self.job_dir), QUrl.TolerantMode))
+    @staticmethod
+    def open_folder(folder):
+        QDesktopServices.openUrl(QUrl("file:{}".format(folder), QUrl.TolerantMode))
 
     def open_basecamp_page(self):
         QDesktopServices.openUrl(QUrl(self.basecamp_url))
